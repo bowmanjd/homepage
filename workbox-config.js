79d3424fd5f310e1004aeb0433ea3fb26d129c3b
Jonathan Bowman <git@bowmanjd.com>
2025-07-27 19:43:20 -0400

    Better service worker registration

--- workbox-config.js
+++ workbox-config.js
@@ -1,14 +1,25 @@
 module.exports = {
 	globDirectory: "public/",
-	globPatterns: ["**/*.{html,json,svg}"],
+	globPatterns: ["**/*.{html,json,svg,css,js}"],
 	swDest: "public/sw.js",
 	skipWaiting: true,
 	clientsClaim: true,
 	runtimeCaching: [
 		{
-			urlPattern: /\.html$/,
+			urlPattern: /\.(?:html|css|js)$/,
 			handler: "StaleWhileRevalidate",
 		},
+		{
+			urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/,
+			handler: "CacheFirst",
+			options: {
+				cacheName: "images",
+				expiration: {
+					maxEntries: 60,
+					maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+				},
+			},
+		}
 	],
 	ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 };
